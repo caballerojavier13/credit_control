1975ec39b773f36792e764d615bc71a67ea6ef70
Javier Caballero <caballerojavier13@gmaill.com>
2015-06-23 23:54:15 -0300

    Ocultar las rutas para crear usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,27 @@
 ControlCredito::Application.routes.draw do
-  get "sessions/new"
-  get "sessions/create"
-  get "sessions/destroy"
-  get "users/new"
-  get "users/create"
+
   resources :quotes
 
   resources :sales
+
+  resources :people
 
   get '/sales/delete/:id', to: 'sales#delete', as: 'delete_sale'
 
   get '/people/delete/:id', to: 'people#delete', as: 'delete_person'
 
-
   get '/sales/quotes/:sale_id', to: 'quotes#quotes_sale', as: 'quotes_sale'
-  post '/sales/quotes/pay/:id', to: 'quotes#pay', as: 'pay_quote'
 
-  resources :people
+  post '/sales/quotes/pay/:id', to: 'quotes#pay', as: 'pay_quote'
 
   root 'static_pages#home'
 
   get '/login' => 'sessions#new'
+
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
+  #get '/signup' => 'users#new'
+  #post '/users' => 'users#create'
 
 end
