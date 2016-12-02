34b0b69b8dce8ec4f3294c2ccd2b604490b36490
Javier Caballero <caballerojavier13@gmail.com>
2016-12-02 00:01:40 -0300

    Sacar el ignorar el secret key, agregar new relic

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ControlCredito::Application.config.secret_key_base = '483d14636c71b0a40d30c2bf4a9f802cf4ee2630fd8216bfa9691c5dea7d8b1f92f3fc2c12dd3d7c1e2d5192a8d3d9f45bb00027e31116c3e9f31f8928aa4d88'
